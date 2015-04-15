bfc989697930fa2a9442dc4c4074847b744e8997
Jeff Reichenberg <Jeffrey.Reichenberg@ge.com>
2015-04-15 14:40:35 -0700

    Testing machinations

--- test/px-app-nav-spec.js
+++ test/px-app-nav-spec.js
@@ -42,11 +42,11 @@ afterEach(function (done) {
 describe('Instancing component from provided DOM data attribute', function () {
 
     before(function() {
-        driver.get('fixture.html');
-        return driver.wait(webdriver.until.elementLocated(webdriver.By.css("px-app-nav li")), 10000);
+        driver.get('fixture.html?dom=shady');
+        return driver.wait(webdriver.until.elementLocated(webdriver.By.css("px-app-nav li")), 3000);
     });
 
-    it('should automatically instance a px-app-nav from the expected DOM', function () {
+    it('should instance a px-app-nav from the expected DOM', function () {
         return chai.expect(
             driver.findElement(webdriver.By.css("px-app-nav"))
         ).to.eventually.exist;
@@ -57,4 +57,66 @@ describe('Instancing component from provided DOM data attribute', function () {
             driver.findElements(webdriver.By.css("px-app-nav li"))
         ).to.eventually.have.length(4);
     });
+
+    it('should hide subitems by default', function () {
+        return chai.expect(
+            driver.findElement(webdriver.By.css("px-app-nav ul > li > ul")).getAttribute("class")
+        ).to.eventually.contain("visuallyhidden");
+    });
 });
+
+
+describe('Interaction', function () {
+
+    it('should be expanded by default', function () {
+        return chai.expect(
+            driver.findElement(webdriver.By.css("px-app-nav")).getSize()
+        ).to.eventually.have.property('width', 219);
+    });
+
+    it('should collapse when the toggle is clicked from an expanded state', function () {
+        driver.findElement(webdriver.By.css("px-app-nav button")).click();
+        driver.wait(webdriver.until.elementLocated(webdriver.By.css("px-app-nav.navbar--collapsed")), 2000);
+        return chai.expect(
+            driver.findElement(webdriver.By.css("px-app-nav")).getSize()
+        ).to.eventually.have.property('width', 55);
+    });
+
+    it('should hide the text labels when collapsed', function () {
+        return chai.expect(
+            driver.findElement(webdriver.By.css("px-app-nav li a span")).getSize()
+        ).to.eventually.have.property('width', 1);
+    });
+
+    it('should expand when the toggle is clicked from a collapsed state', function () {
+        driver.findElement(webdriver.By.css("px-app-nav button")).click();
+        driver.wait(webdriver.until.elementLocated(webdriver.By.css("px-app-nav:not(.navbar--text-hidden)")), 2000);
+        return chai.expect(
+            driver.findElement(webdriver.By.css("px-app-nav")).getSize()
+        ).to.eventually.have.property('width', 219);
+    });
+
+    it('should show subitems when a parent item is clicked', function () {
+        driver.findElement(webdriver.By.css("px-app-nav ul > li > a")).click();
+        return chai.expect(
+            driver.findElement(webdriver.By.css("px-app-nav ul > li > ul")).getAttribute("class")
+        ).to.eventually.not.contain("visuallyhidden");
+    });
+});
+
+describe('Navigation', function () {
+
+    it('should navigate when clicking on an item', function () {
+        driver.findElement(webdriver.By.css("px-app-nav a[href='#tab2']")).click();//click cases
+        return chai.expect(
+            driver.getCurrentUrl()
+        ).to.eventually.contain("#tab2");
+    });
+
+    it('should mark the selected item as selected', function () {
+        return chai.expect(
+            driver.findElement(webdriver.By.css("px-app-nav a[href='#tab2'].active"))
+        ).to.eventually.exist;
+    });
+});
+
