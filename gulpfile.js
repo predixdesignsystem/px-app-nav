ddb5d9bc40662d5a7950ecddc7f4c85c7194610f
Martin Wragg <mdwragg@gmail.com>
2016-08-03 17:22:59 -0700

    Add gulp-bump task

--- gulpfile.js
+++ gulpfile.js
@@ -60,4 +60,22 @@ gulp.task('serve', ['default'], function() {
 
 });
 
+gulp.task('bump:patch', function(){
+  gulp.src(['./bower.json', './package.json'])
+  .pipe(bump({type:'patch'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:minor', function(){
+  gulp.src(['./bower.json', './package.json'])
+  .pipe(bump({type:'minor'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:major', function(){
+  gulp.src(['./bower.json', './package.json'])
+  .pipe(bump({type:'major'}))
+  .pipe(gulp.dest('./'));
+});
+
 gulp.task('default', gulpSequence('sass'));
